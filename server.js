c855ca918daf2fc757dfc966abd96f65f11c9f32
pelle <wiepiegames1@gmail.com>
2017-10-31 15:01:27 +0100

    Init commit

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const hbs = require('hbs');
+const ping = require('ping');
+const fs = require('fs');3
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+  next();
+});
+
+app.use((req, res, next) => {
+  res.render('maintenance.hbs', {
+    siteTitle: 'maintenance',
+    pageTitle: 'We are currently closed!',
+  });
+});
+
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    siteTitle: 'Home Site',
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome friend!',
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('about.hbs', {
+    pageTitle: 'About Page',
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Bad request',
+  });
+});
+
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
